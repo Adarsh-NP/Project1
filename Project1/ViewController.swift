e3732f95826c3c206f6bcaeabcb339239d496168
Adarsh Pandey <adarsh.narayan@uber.com>
2023-02-21 16:44:11 +0530

    Project 1 with Challenges

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -5,13 +5,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var pic = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Storm Viewer"
         // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pic.append(item)
+            }
+        }
+        
+        pic = pic.sorted{ $0 < $1}
+        
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pic.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pic[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pic[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+            vc.totalImages = pic.count
+            vc.currentImage = indexPath.row + 1
+        }
+    }
 
 }
 
